5df96961de5258442642d981d9c1b5253790e552
jlixerkun <jlixerkun@gmail.com>
2022-11-02 01:39:40 -0500

    initialcommit

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  import Counter from './lib/Counter.svelte'
+  import Header from './lib/Header.svelte';
+</script>
+<main>
+  <Header status="main" />
+  
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,59 @@
+<script>
+  export let status = "default";
+  import amarilogo from "../assets/amarilogo.svg";
+</script>
+
+<header class={status}>
+  <div class="container">
+    <a href="/" title="inicio" rel="noreferrer" class="logo">
+      <img src={amarilogo} alt="Logo amarilo" />
+    </a>
+    <nav class="menu">
+      <ul>
+        <li><a href="/proyectos">Proyectos</a></li>
+        <li><a href="/herramientas">Herramientas</a></li>
+        <li><a href="/mi-cuenta">Mi cuenta</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
+
+<style>
+  header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+  }
+  .main .container{
+    border-color: #334488; 
+    display: grid;
+    grid-template-columns:1fr 7fr ;
+  }
+  .main .container > * {
+    align-self: center;
+  }
+  .menu {
+    display: inline-block;
+  }
+  .menu ul {
+    list-style: none;
+  }
+
+  .menu ul li {
+    display: inline-block;
+  }
+  
+  .menu ul li a {
+    padding: 2rem;
+  }
+  .container {
+    width: 90%;
+    margin: auto;
+    /* border: 1px solid rgb(198, 11, 11); */
+  }
+
+  .logo img {
+    max-width: 130px;
+  }
+</style>
